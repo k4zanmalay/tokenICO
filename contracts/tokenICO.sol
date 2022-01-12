19b1248b1592d3cd8add07da4c5e05dae61a2091
k4zanmalay <k4zanmalay@yandex.ru>
2022-01-12 18:14:22 +0300

    Initial commit

---
+++ contracts/tokenICO.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TTT is ERC20, Ownable {
+    mapping(address => bool) private whitelisted;
+    address public ICOAddress;
+
+    constructor() ERC20("TimelockTransferToken", "TTT") {
+        _mint(_msgSender(), 1000000 * 10 ** decimals());
+        whitelisted[_msgSender()] = true;
+        }
+
+    function whitelist(address member) onlyOwner public {
+        whitelisted[member] = true;
+    }
+
+    function createICO(uint supply) public onlyOwner {
+        ICOAddress = address(new ICO(address(this)));
+        transfer(ICOAddress, supply);
+        IICO(ICOAddress).setICOState(true);
+        whitelisted[ICOAddress] = true;
+    }
+
+    function setICOState(bool state) onlyOwner public {
+        IICO(ICOAddress).setICOState(state);
+    }
+
+    function transfer(address recipient, uint amount) public override returns(bool){
+        if(IICO(ICOAddress).ICOActive())
+            require(whitelisted[_msgSender()], "Not authorized to transfer until the end of ICO");
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+}
+
+contract ICO {
+    address tokenAddress;
+    bool ICOActive;
+    uint8 private constant TOKENS_PER_ETH_STAGE1 = 42;
+    uint8 private constant TOKENS_PER_ETH_STAGE2 = 21;
+    uint8 private constant TOKENS_PER_ETH_STAGE3 = 8;
+    uint private stage1;
+    uint private stage2;
+    uint private stage3;
+
+
+    constructor(address token) {
+        stage1 = block.timestamp + 3 days;
+        stage2 = block.timestamp + 30 days + stage1;
+        stage3 = block.timestamp + 2 weeks + stage2;
+        tokenAddress = token;
+    }
+
+    function setICOState(bool state) external {
+        ICOActive = state;
+    }
+
+    receive () external payable {
+        require(IERC20(tokenAddress).balanceOf(address(this)) > 0, "Out of tokens");
+        require(ICOActive, "ICO closed");
+        require(block.timestamp < stage3, "ICO closed");  
+        if(block.timestamp >= stage2){
+            IERC20(tokenAddress).transfer(msg.sender, TOKENS_PER_ETH_STAGE3 * msg.value / 1e18);
+            return;
+        }
+        if(block.timestamp >= stage1){
+            IERC20(tokenAddress).transfer(msg.sender, TOKENS_PER_ETH_STAGE2 * msg.value / 1e18);
+            return;
+        }
+        IERC20(tokenAddress).transfer(msg.sender, TOKENS_PER_ETH_STAGE1 * msg.value/ 1e18);
+    }
+}
+
+interface IICO {
+    function ICOActive() external view returns(bool);
+    function setICOState(bool) external;
+}
+
